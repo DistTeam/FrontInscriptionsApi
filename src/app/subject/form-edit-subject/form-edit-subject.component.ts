b437fffd42b4563d6a078d87acf53889299eeac8
Emily <emily.moreno@uptc.edu.co>
2023-03-27 19:01:25 -0500

    Se crea el componente editar materia

---
+++ src/app/subject/form-edit-subject/form-edit-subject.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-edit-subject',
+  templateUrl: './form-edit-subject.component.html',
+  styleUrls: ['./form-edit-subject.component.css']
+})
+export class FormEditSubjectComponent {
+
+}
