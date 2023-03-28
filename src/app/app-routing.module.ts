e0a9fa2391e158b7393f1841bd5bd6dd8fb779a8
Emily <emily.moreno@uptc.edu.co>
2023-03-27 20:52:22 -0500

    Se integra la tabla para vizualizar materias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import {FormStudentComponent} from "./Students/form-create-student/form-student.
 import {FormEditStudentComponent} from "./Students/form-edit-student/form-edit-student.component";
 import {FormSubjectComponent} from "./subject/form-create-subject/form-subject.component";
 import {FormEditSubjectComponent} from "./subject/form-edit-subject/form-edit-subject.component";
-
+import {ShowInspectionComponent} from "./subject/show-inspection/show-inspection.component";
 
 const routes: Routes = [
   { path: 'create-student', component: FormStudentComponent},
   {path:'edit-student',component: FormEditStudentComponent},
   {path:'create-subject',component:FormSubjectComponent},
-  {path:'edit-subject',component: FormEditSubjectComponent}
+  {path:'edit-subject',component: FormEditSubjectComponent},
+  {path:'view-subject',component: ShowInspectionComponent}
 ];
 @NgModule({
   declarations: [],
