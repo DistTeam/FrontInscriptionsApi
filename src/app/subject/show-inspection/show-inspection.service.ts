90d4ad166d87cd04f669537c5ec35374508d6d35
Emily <emily.moreno@uptc.edu.co>
2023-03-29 02:25:26 -0500

    se crea Servicio de materias y se implenetan sus metodos

--- src/app/subject/show-inspection/show-inspection.service.ts
+++ src/app/subject/show-inspection/show-inspection.service.ts
@@ -12,8 +12,13 @@ export class ShowInspectionService {
 
   constructor(private http: HttpClient) { }
 
-  getSubject(pageNumber: number = 50, pageSize: number = 10, sortOrder: string = "", sortBy: string = "", searchString: string = ""): Observable<any> {
+  /*getSubject(pageNumber: number = 50, pageSize: number = 10, sortOrder: string = "", sortBy: string = "", searchString: string = ""): Observable<any> {
     let url = `${this.baseUrl}/withSorts?pageNumber=${pageNumber}&pageSize=${pageSize}&sortOrder=${sortOrder}&sortBy=${sortBy}&searchString=${searchString}`;
+    return this.http.get<any[]>(url, {observe: 'response'});
+  }*/
+
+  getSubjects(page: number, pageSize: number, sortBy: string, sortOrder: string):Observable<any> {
+    const url = `https://localhost:7243/api/Subjects/withSorts?sortBy=${sortBy}&sortOrder=${sortOrder}&page=${page}&pageSize=${pageSize}`;
     return this.http.get<any[]>(url, {observe: 'response'});
   }
 }
