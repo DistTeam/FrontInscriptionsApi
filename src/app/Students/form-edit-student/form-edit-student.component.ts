f852221604a7c3a87a1f2c3f9e721e32abf577ee
Andres Torres <santiago.ojedatorrres@gmail.com>
2023-04-17 13:44:08 -0500

    Se ajustan las imagenes al ser mostradas en el recudaro donde se arrastran

--- src/app/Students/form-edit-student/form-edit-student.component.ts
+++ src/app/Students/form-edit-student/form-edit-student.component.ts
@@ -76,7 +76,7 @@ export class FormEditStudentComponent {
   }
 
   editStudent(form: NgForm) {
-    if (this.imageUrl != null && this.imageFile != null) {
+    if (this.imageUrl != null && this.imageFile != null && this.service.formDataStudent.studentPhoto.includes("https://almacenamientoproyecto.blob.core.windows.net") == false) {
       this.service.formDataStudent.studentPhoto = this.imageUrl;
       console.log(this.service.formDataStudent);
       this.service.uploadImg(this.imageFile).subscribe(
@@ -99,7 +99,18 @@ export class FormEditStudentComponent {
           this.toastr.error(err.toString());
         }
       );
-    } else {
+    } else if(this.service.formDataStudent.studentPhoto.includes("https://almacenamientoproyecto.blob.core.windows.net") == true){
+      this.service.putStudent().subscribe(
+        (res: any) => {
+          this.toastr.success('Estudiante actualizado con exito', 'Inscripciones UPTC');
+          this.resetForm(form);
+        },
+        (err: any) => {
+          this.toastr.error(err);
+        }
+      );
+    }
+    else{
       this.service.formDataStudent.studentPhoto = "NULL";
       this.service.putStudent().subscribe(
         (res: any) => {
