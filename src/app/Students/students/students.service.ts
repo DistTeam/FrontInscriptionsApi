a483eea872fe6c52810ef525c43cf8ef4bfde414
Andres Torres <santiago.ojedatorrres@gmail.com>
2023-04-22 23:16:27 -0500

    Se actualizo el gateway para el uso del redireccionamiento

--- src/app/Students/students/students.service.ts
+++ src/app/Students/students/students.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 
 @Injectable({
@@ -7,11 +7,18 @@ import { map, Observable } from 'rxjs';
 })
 export class StudentsService {
 
-  private baseUrl = 'http://104.210.221.168/api/Students';
+  private baseUrl: string | null = 'http://104.210.221.168/api/Students';
 
   constructor(private http: HttpClient) { }
 
   getStudents(pageNumber: number = 50, pageSize: number = 10, sortOrder: string = "", sortBy: string = "", searchString: string = ""): Observable<any> {
+    if (this.baseUrl != null) {
+      this.http.head(this.baseUrl, {observe: 'response'})
+        .pipe(
+          map(response => response.headers.get('Location'))
+        )
+        .subscribe(redirectUrl => this.baseUrl = redirectUrl);
+    }
     let url = `${this.baseUrl}/withSorts?pageNumber=${pageNumber}&pageSize=${pageSize}&sortOrder=${sortOrder}&sortBy=${sortBy}&searchString=${searchString}`;
     return this.http.get<any[]>(url, {observe: 'response'});
   }
--- src/app/subject/show-inspection/show-inspection.service.ts
+++ src/app/subject/show-inspection/show-inspection.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 
 @Injectable({
@@ -7,11 +7,18 @@ import { map, Observable } from 'rxjs';
 })
 export class ShowInspectionService {
 
-  private baseUrl = 'http://104.210.221.168/api/Subjects';
+  private baseUrl: string | null = 'http://104.210.221.168/api/Subjects';
 
   constructor(private http: HttpClient) { }
 
   getSubjects(sortBy: string = "Id", sortOrder: string = "asc", page: number = 1, pageSize: number = 10): Observable<any> {
+    if (this.baseUrl != null) {
+      this.http.head(this.baseUrl, {observe: 'response'})
+        .pipe(
+          map(response => response.headers.get('Location'))
+        )
+        .subscribe(redirectUrl => this.baseUrl = redirectUrl);
+    }
     let url = `${this.baseUrl}/withSorts?sortBy=${sortBy}&sortOrder=${sortOrder}&page=${page}&pageSize=${pageSize}`;
     return this.http.get<any[]>(url, { observe: 'response' });
   }
