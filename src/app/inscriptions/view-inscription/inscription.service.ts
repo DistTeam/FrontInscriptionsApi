5d624f0125b6a959cc3d331640f60981afce98a5
Andres Torres <santiago.ojedatorrres@gmail.com>
2023-04-22 23:38:57 -0500

    Se actualizo el codigo para manejar el gateway

--- src/app/inscriptions/view-inscription/inscription.service.ts
+++ src/app/inscriptions/view-inscription/inscription.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,21 @@ export class InscriptionService {
 
   constructor(private http: HttpClient) { }
   getInscriptions(pageNumber: number = 50, pageSize: number = 10, sortOrder: string = "asc", sortBy: string = "", searchString: string = ""): Observable<any> {
+    this.baseUrlInscriptionsGetAll = "http://104.210.221.168/api/Inscriptions"
+    console.log(this.baseUrlInscriptionsGetAll);
+    this.http.get(this.baseUrlInscriptionsGetAll, {
+      headers: new HttpHeaders({
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+      }),
+      observe: 'response',
+      responseType: 'text',
+    }).pipe(
+      tap((res) => {
+        const redirectedUrl = res.url;
+        console.log('Redirected URL:', redirectedUrl);
+      })
+    ).subscribe();
     console.log("entro"  + " page " + pageSize  + " como " + sortOrder  + " por " + sortBy + " dsearch " +searchString)
     let url = `${this.baseUrlInscriptionsGetAll}/all?pageNumber=${pageNumber}&pageSize=${pageSize}&sortOrder=${sortOrder}&sortBy=${sortBy}`;
     return this.http.get<any[]>(url, {observe: 'response'});
