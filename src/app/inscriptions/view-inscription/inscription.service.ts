00522134e9603b2366824f90deabef6e1d3ad9aa
Andres Torres <santiago.ojedatorrres@gmail.com>
2023-04-23 03:03:13 -0500

    Se actualizo el codigo para obtener el redireccionamiento

--- src/app/inscriptions/view-inscription/inscription.service.ts
+++ src/app/inscriptions/view-inscription/inscription.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, tap} from "rxjs";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,15 @@ export class InscriptionService {
   getInscriptions(pageNumber: number = 50, pageSize: number = 10, sortOrder: string = "asc", sortBy: string = "", searchString: string = ""): Observable<any> {
     this.baseUrlInscriptionsGetAll = "http://104.210.221.168/api/Inscriptions";
     console.log(this.baseUrlInscriptionsGetAll);
-    this.http.get(this.baseUrlInscriptionsGetAll, {observe: 'response'}).pipe(
-      tap(response => {
-        this.baseUrlInscriptionsGetAll = response.headers.get('Location');
-      })
-    ).subscribe();
+    this.http.get(this.baseUrlInscriptionsGetAll, { observe: 'response' })
+      .toPromise()
+      .then(response => {
+        if (response && response.headers) {
+          return response.headers.get('location');
+        } else {
+          throw new Error('No se pudo obtener la dirección de redirección');
+        }
+      });
     console.log(this.baseUrlInscriptionsGetAll);
     console.log("entro" + " page " + pageSize + " como " + sortOrder + " por " + sortBy + " dsearch " + searchString)
     let url = `${this.baseUrlInscriptionsGetAll}/all?pageNumber=${pageNumber}&pageSize=${pageSize}&sortOrder=${sortOrder}&sortBy=${sortBy}`;
