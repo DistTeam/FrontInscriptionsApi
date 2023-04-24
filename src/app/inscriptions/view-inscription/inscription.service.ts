5e9548435a5cd2cea1c86250e4c391e15d7e1771
Andres Torres <santiago.ojedatorrres@gmail.com>
2023-04-24 16:26:29 -0500

    Se ajusta para uso local del front

--- src/app/inscriptions/view-inscription/inscription.service.ts
+++ src/app/inscriptions/view-inscription/inscription.service.ts
@@ -12,18 +12,6 @@ export class InscriptionService {
   }
 
   getInscriptions(pageNumber: number = 50, pageSize: number = 10, sortOrder: string = "asc", sortBy: string = "", searchString: string = ""): Observable<any> {
-    this.baseUrlInscriptionsGetAll = "http://104.210.221.168/api/Inscriptions";
-    console.log(this.baseUrlInscriptionsGetAll);
-    this.http.get(this.baseUrlInscriptionsGetAll, { observe: 'response' })
-      .toPromise()
-      .then(response => {
-        if (response && response.headers) {
-          return response.headers.get('location');
-        } else {
-          throw new Error('No se pudo obtener la dirección de redirección');
-        }
-      });
-    console.log(this.baseUrlInscriptionsGetAll);
     console.log("entro" + " page " + pageSize + " como " + sortOrder + " por " + sortBy + " dsearch " + searchString)
     let url = `${this.baseUrlInscriptionsGetAll}/all?pageNumber=${pageNumber}&pageSize=${pageSize}&sortOrder=${sortOrder}&sortBy=${sortBy}`;
     return this.http.get<any[]>(url, {observe: 'response'});
